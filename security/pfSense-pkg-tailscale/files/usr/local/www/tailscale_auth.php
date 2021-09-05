4f37dae0c0b972a9a1230a29befa31cc36c5ecdd
Denton Gentry <dgentry@tailscale.com>
2021-09-05 16:34:57 -0700

    Run "tailscale up" with arguments.
    Remove "tailscale web" because I can't get the login button to work.

--- security/pfSense-pkg-tailscale/files/usr/local/www/tailscale_auth.php
+++ security/pfSense-pkg-tailscale/files/usr/local/www/tailscale_auth.php
@@ -27,26 +27,75 @@ $guiuser = getUserEntry($_SESSION['Username']);
 $username = $guiuser['name'];
 $is_admin = isAdminUID($username);
 
-putenv("REQUEST_METHOD=GET");
-putenv("SERVER_PROTOCOL=HTTP/1.1");
-putenv("PFSENSE_USERNAME=" . $username);
-if ($is_admin) {
-	putenv("PFSENSE_IS_ADMIN=1");
+global $config, $tailscale_config;
+if (is_array($config['installedpackages']['tailscale'])) {
+	$tailscale_config =& $config['installedpackages']['tailscale']['config'][0];
+} else {
+	$tailscale_config = array();
+}
+
+$tailscaleup = "/usr/local/bin/tailscale --socket=/run/tailscale/tailscaled.sock";
+if ($tailscale_config['enable'] != "on") {
+	$tailscaleup .= " down";
+} else {
+	$tailscaleup .= " up --reset";
+
+	if ($tailscale_config['advertise_lan_route'] == 'on') {
+		$iflist = get_configured_interface_list();
+		foreach ($iflist as $if => $ifname) {
+			if ($ifname == "lan") {
+				$if_ipv4 = get_interface_ip($if);
+				$if_snbitsv4 = get_interface_subnet($if);
+				$if_subnet = gen_subnet($if_ipv4, $if_snbitsv4);
+				$tailscaleup .= " --advertise-routes=" . $if_subnet . "/" . $if_snbitsv4;
+			}
+		}
+	}
+	if ($tailscale_config['advertise_exit_node'] == 'on') {
+		$tailscaleup .= " --advertise-exit-node=true";
+	}
+
+	$tailscaleup .= " --accept-dns=false --accept-routes=true --reset";
 }
 
 include("head.inc");
-$out = shell_exec("/usr/local/bin/tailscale --socket=/run/tailscale/tailscaled.sock web -cgi");
-$lines = explode(PHP_EOL, $out);
+if ($is_admin) {
+	echo "<div>" . $tailscaleup . "</div>";
 
-/* chop off the 200 Status and other lines until we find <!doctype html> */
-while ($line = array_shift($lines))
-{
-	if (strpos($line, 'doctype') !== false) {
-		break;
+	$fds = array(
+		0 => array("pipe", "r"),
+		1 => array("pipe", "w"),
+		2 => array("pipe", "w")
+	);
+	$process = proc_open($tailscaleup, $fds, $pipes);
+	if (is_resource($process)) {
+		stream_set_blocking($pipes[1],0);
+		stream_set_blocking($pipes[2],0);
+		echo "<div>";
+		$out = "";
+		$loop = true;
+		while ($loop) {
+			$out .= fread($pipes[1], 1024);
+			$out .= fread($pipes[2], 1024);
+			if (strpos($out, "https://") !== false) {
+				$loop = false;
+			} else if (feof($pipes[1]) && feof($pipes[2])) {
+				$loop = false;
+			} else {
+				usleep(500000);
+			}
+		}
+		echo $out . "</div>";
+	} else {
+		echo "<div>Command failed</div>";
 	}
+	fclose($pipes[0]);
+	fclose($pipes[1]);
+	fclose($pipes[2]);
+	proc_close($process);
+} else {
+	echo "Error: " . $username . " is not an Admin.";
 }
-
-echo implode("\r\n", $lines);
 
 include("foot.inc");
 ?>
